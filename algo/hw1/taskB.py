30382aa7abd9b9e2e4246269cdbb18abbeafbb1f
alexysxeightn <64883422+alexysxeightn@users.noreply.github.com>
2021-09-19 20:39:31 +0900

    Create taskB.py

---
+++ algo/hw1/taskB.py
@@ -0,0 +1,34 @@
+n = int(input())
+L = list(map(int, input().split()))
+     
+# Сортировка слиянием
+     
+def merge(left_arr, right_arr):
+    n, m = len(left_arr), len(right_arr)
+    i, j = 0, 0
+    merge_arr = [0] * (n + m)
+        
+    while i + j < n + m:
+        if j == m or (i < n and left_arr[i] < right_arr[j]):
+            merge_arr[i + j] = left_arr[i]
+            i += 1
+        else:
+            merge_arr[i + j] = right_arr[j]
+            j += 1
+        
+    return merge_arr
+     
+def merge_sort(arr):
+    n = len(arr)
+    if n == 1:
+        return arr
+            
+    median = n // 2
+    left_arr, right_arr = arr[:median], arr[median:]
+    left_arr, right_arr = merge_sort(left_arr), merge_sort(right_arr)
+        
+    return merge(left_arr, right_arr)
+     
+L = merge_sort(L)
+     
+print(*L)
