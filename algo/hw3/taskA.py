ccc9a9c003f8ce1a87bc0c2425bd98bfe886d4ee
alexysxeightn <64883422+alexysxeightn@users.noreply.github.com>
2021-09-26 15:51:03 +0900

    Create taskA.py

---
+++ algo/hw3/taskA.py
@@ -0,0 +1,31 @@
+def lower_bound(array, n, x):
+    left, right = -1, n
+    while left < right - 1:
+        m = (left + right) // 2
+        if x <= array[m]:
+            right = m
+        else:
+            left = m
+    return right
+
+n, k = map(int, input().split())
+array = [*map(int, input().split())]
+queries = [*map(int, input().split())]
+
+for query in queries:
+
+    lb, ub = lower_bound(array, n, query), lower_bound(array, n, query + 1)
+    
+    if lb == n: 
+        lb -= 1
+    
+    if ub == n:
+        ub -= 1
+    
+    if array[lb] != query and lb != 0:
+        lb -= 1
+    
+    if abs(array[lb] - query) <= abs(array[ub] - query):
+        print(array[lb])
+    else:
+        print(array[ub])
