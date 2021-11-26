a79662ff5146657f2b062a32748f60c48e2a9d59
alexysxeightn <64883422+alexysxeightn@users.noreply.github.com>
2021-11-26 12:52:50 +0900

    Update taskC.py

--- algo/hw11/taskC.py
+++ algo/hw11/taskC.py
@@ -1,43 +1,36 @@
-INF = 10 ** 9
-MAX_W = 10 ** 5
-
-def floyd(edges, n):
-    dist = [INF] * n
-    dist[0] = 0
-    p = [-1] * n
-    
-    num_edges = len(edges)
+    INF = 10 ** 5
+     
+    def floyd(d, n):
+        next = [[0] * n for _ in range(n)]
+        for u in range(n):
+            for v in range(n):
+                next[u][v] = v
+        
+        for k in range(n):
+            for u in range(n):
+                for v in range(n):
+                    if d[u][k] == INF or d[k][v] == INF:
+                        continue
+                    if d[u][v] > d[u][k] + d[k][v]:
+                        d[u][v] = max(-INF, d[u][k] + d[k][v])
+                        next[u][v] = next[u][k]
+                        if u == v and d[u][v] < 0:
+                            path = [v + 1]
+                            cur = next[v][v]
+                            while cur != v:
+                                path.append(cur + 1)
+                                cur = next[cur][v]
+                            return path
+        return False
+     
+    n = int(input())
+    graph = [0] * n
     for i in range(n):
-        next = -1
-        for j in range(num_edges):
-            u, v, w = edges[j]
-            if dist[v] > dist[u] + w:
-                dist[v] = max(-INF, dist[u] + w)
-                p[v] = u
-                next = v
-    if -1 == next:
-        return
-
-    y = next
-    for _ in range(n):
-        y = p[y]
-    path = [y + 1]    
-    cur = p[y]
-    while cur != y:
-        path.append(cur + 1)
-        cur = p[cur]
-    return path[::-1]
-
-n = int(input())
-edges = []
-for i in range(n):
-    for j, w in enumerate(map(int, input().split())):
-        if w != MAX_W:
-            edges.append((i, j, w))
-res = floyd(edges, n)
-if res:
-    print('YES')
-    print(len(res))
-    print(*res)
-else:
-    print('NO')
+        graph[i] = [*map(int, input().split())]
+    res = floyd(graph, n)
+    if res:
+        print('YES')
+        print(len(res))
+        print(*res)
+    else:
+        print('NO')
