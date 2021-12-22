871c8b2850c162be53c6b31c20c837ba3923e30f
alexysxeightn <64883422+alexysxeightn@users.noreply.github.com>
2021-12-22 17:18:29 +0900

    Update taskB.py

--- algo/hw15/taskB.py
+++ algo/hw15/taskB.py
@@ -1,10 +1,8 @@
-NUMBERS = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-OPERANDS = ('+', '-', '*', '(', ')', '.')
-
-flag_of_wrong = False
+NUMBERS = '0123456789'
+OPERANDS = '+-*().'
 
 def is_number(s):
-    if s == '0':
+    if '0' == s:
         return True
     if s[0] not in NUMBERS[1:]:
         return False
@@ -15,13 +13,8 @@ def is_number(s):
 
 class Lexer:
     def __init__(self, s):
-        global flag_of_wrong
+        self.flag_of_wrong = False
         self.tokens = []
-        
-        if s.count('(') != s.count(')'):
-            flag_of_wrong = True
-        
-        s = s.replace(' ', '')
         
         num_str = ''
         for symbol in s:
@@ -33,27 +26,28 @@ class Lexer:
                     num_str = ''
                 self.tokens.append(symbol)
             else:
-                flag_of_wrong = True
+                self.flag_of_wrong = True
                 break
         
     def token_generator(self):
         for token in self.tokens:
             yield token
 
-class Parser:
-    def __init__(self, Lexer):
-        self.tokens = Lexer.token_generator()
+class Parser(Lexer):
+    def __init__(self, s):
+        super().__init__(s)
         self.cur_token = None
+        self.tokens_gen = self.token_generator()
     
     def next_token(self):
-        self.cur_token = next(self.tokens)
+        self.cur_token = next(self.tokens_gen)
     
     def parse_expr(self):
         result = self.parse_sum()
-        while self.cur_token == '+' or self.cur_token == '-':
+        while self.cur_token in '+-':
             c = self.cur_token
             self.next_token()
-            if c == '+':
+            if '+' == c:
                 result = result + self.parse_sum()
             else:
                 result = result - self.parse_sum()
@@ -61,39 +55,39 @@ class Parser:
             
     def parse_sum(self):
         result = self.parse_product()
-        while self.cur_token == '*':
+        while '*' == self.cur_token:
             self.next_token()
             result = result * self.parse_product()
         return result
         
     def parse_product(self):
+        
         if is_number(self.cur_token):
             number = self.cur_token
             self.next_token()
-            if self.cur_token == '(':
-                raise Exception()
+            assert self.cur_token != '('
             return int(number)
-        elif self.cur_token == '(':
+            
+        elif '(' == self.cur_token:
             self.next_token()
             answer = self.parse_expr()
-            assert self.cur_token == ')'
+            assert ')' == self.cur_token
             self.next_token()
             return answer
+        
         else:
             raise Exception()
 
     def parse(self):
-        if flag_of_wrong:
-            return 'WRONG'
-        else:
+        try:
+            assert not self.flag_of_wrong
             self.next_token()
-            return self.parse_expr()
-
+            result = self.parse_expr()
+            assert '.' == self.cur_token
+            return result
+        except:
+            return 'WRONG'
+            
 s = str(input())
-lexer = Lexer(s)
-parser = Parser(lexer)
-
-try:
-    print(parser.parse())
-except:
-    print('WRONG')
+parser = Parser(s)
+print(parser.parse())
