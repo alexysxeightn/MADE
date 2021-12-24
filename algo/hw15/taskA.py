66b5ca1ef6b39fecf1ccdcfb086d6a851ee8b793
alexysxeightn <64883422+alexysxeightn@users.noreply.github.com>
2021-12-24 14:35:15 +0900

    Update taskA.py

--- algo/hw15/taskA.py
+++ algo/hw15/taskA.py
@@ -1,4 +1,21 @@
-NUMBERS = '0123456789'
+class Token:
+    def __init__(self, s, type):
+        self.value = s
+        self.type = type
+    
+    def __str__(self):
+        return str(self.value)
+    
+    def __repr__(self):
+        print(self.value, end=' ')
+
+class Number_Token(Token):
+    def __init__(self, s):
+        super().__init__(int(s), 'number')
+
+class Operand_Token(Token):
+    def __init__(self, s):
+        super().__init__(s, 'operand')
 
 class Lexer:
     def __init__(self, s):
@@ -6,14 +23,21 @@ class Lexer:
         
         num_str = ''
         for symbol in s:
-            if symbol in NUMBERS:
+            if is_digit(symbol):
                 num_str += symbol
             else:
                 if num_str:
-                    self.tokens.append(num_str)
+                    self.tokens.append(Number_Token(num_str))
                     num_str = ''
-                self.tokens.append(symbol)
+                self.tokens.append(Operand_Token(symbol))
 
-s = input()
-lexer = Lexer(s)
-print(*lexer.tokens[:-1], sep='\n')
+def is_digit(c):
+    return 0 <= ord(c) - ord('0') <= 9 
+
+def main():
+    s = input()
+    lexer = Lexer(s)
+    print(*lexer.tokens[:-1], sep='\n')
+    
+if __name__ == '__main__':
+    main()
